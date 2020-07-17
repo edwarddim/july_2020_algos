5fd9653b6cfe2d6d0c9024c8150a90c98a988ce4
Edward <eddieim@gmail.com>
2020-07-17 11:18:04 -0600

    w2d5 update

--- solutions_week2.js
+++ solutions_week2.js
@@ -131,3 +131,43 @@ function updateInventory(newInv, currInv) {
 }
 
 
+function firstNonRepeatingLetter(string) {
+  var letterDict = {};
+  [...string].forEach(function(letter, index) {
+      if(letter.toLowerCase() in letterDict){
+          letterDict[letter.toLowerCase()] = {
+              'repeat':true,
+              'index':index
+          };
+      }
+      else if(letter.toUpperCase() in letterDict){
+          letterDict[letter.toUpperCase()] = {
+              'repeat':true,
+              'index':index
+          };
+      }
+      else{
+          letterDict[letter] = {
+              'repeat':false,
+              'index':index
+          };
+      }
+
+  })
+  var lowestIndex = string.length;
+  var returnAnswer = '';
+  for(var letter in letterDict){
+      console.log(letterDict[letter])
+      console.log(letter)
+      if(letterDict[letter].repeat){
+          continue;
+      }
+      else{
+          if(letterDict[letter].index < lowestIndex){
+              lowestIndex = letterDict[letter].index
+              returnAnswer = letter;
+          }
+      }
+  }
+  return returnAnswer;
+}
