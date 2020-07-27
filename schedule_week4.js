9126b42677912cda1132dd0154e334baeeb429bd
Edward <eddieim@gmail.com>
2020-07-26 21:13:56 -0600

    start of week 4

---
+++ schedule_week4.js
@@ -0,0 +1,90 @@
+// MON
+/* 
+  Recursively sum an arr of ints
+*/
+
+function sumArr(nums, i = 0) {
+
+}
+
+
+/* 
+  Recursive Sigma
+  Input: integer
+  Output: sum of integers from 1 to Input integer
+*/
+function recursiveSigma(n) {
+
+}
+
+// -----------------------------------------------------------------------------------------------//
+// -----------------------------------------------------------------------------------------------//
+
+// TUE
+/* 
+    Given an array nested with unknown amount of arrays,
+    return the integers all under ONE array
+*/
+
+function recFlatten(arr){
+
+}
+
+// EX. [1,[2,3,[4]],5] => [1,2,3,4,5]
+
+// -----------------------------------------------------------------------------------------------//
+// -----------------------------------------------------------------------------------------------//
+
+// WED
+
+
+/*
+  Recursive Binary Search
+  Input: SORTED array of ints, int value
+  Output: bool representing if value is found
+  Recursively search to find if the value exists, do not loop over every element.
+  Approach:
+  Take the middle item and compare it to the given value.
+  Based on that comparison, narrow your search to a particular section of the array
+*/
+
+function recursiveBinary(arr, target){
+
+}
+
+// -----------------------------------------------------------------------------------------------//
+// -----------------------------------------------------------------------------------------------//
+
+// THUR
+
+/* 
+    Rising Square
+    Given a number return an array filled with the
+    squares of integers up to given number
+
+*/
+function risingSqaures(num){
+
+}
+// EX. 3 => [1,4,9]
+// EX. 5 => [1,4,9,16,25]
+
+
+// -----------------------------------------------------------------------------------------------//
+// -----------------------------------------------------------------------------------------------//
+
+
+// FRI
+
+/*
+    String Subset
+    Given a string, return an array filled
+    with IN-ORDER substrings
+*/
+
+
+function stringSubset(string, arr){
+
+}
+
+// EX. "ABC" => ["", "C", "B", "BC", "A", "AC", "AB", "ABC"]
